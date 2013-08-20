c2ea445e37182fb92293b044356415242efd752d
Anthony Morrissey <ajmorrissey@gmail.com>
2013-08-19 23:34:48 -0700

    initial commit

---
+++ dhcpRenew.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import os
+import sys
+import subprocess
+import time
+import sys
+import datetime
+
+
+if os.geteuid() != 0:
+    print "Script not started as root. Running sudo.."
+    args = ['sudo', sys.executable] + sys.argv + [os.environ]
+    # the next line replaces the currently-running process with the sudo
+    os.execlpe('sudo', *args)
+
+
+def renew_dhcp():
+	""" Main script to renew DHCP
+	"""
+	try:
+		renew_time = int(raw_input("please enter a time in minutes to rewnew dhcp (example: 10): "))
+	except ValueError:
+		print("That was not a number, please try again (example: 10): ")
+		renew_dhcp()
+
+	renew_time = renew_time * 60
+
+	while True:
+		for i in range(renew_time):
+
+			time.sleep(1)
+
+			time_left = str(datetime.timedelta(seconds=renew_time - i))
+
+			sys.stdout.write("\r{0} until next DHCP renewal. ".format(time_left)) 
+			sys.stdout.flush()
+
+		subprocess.call("ipconfig set en1 BOOTP", shell=True)
+		subprocess.call("ipconfig set en1 DHCP", shell=True)
+
+if __name__ == "__main__":
+	renew_dhcp()
